e5fd9ab15648f24c99e52040cedef94b0b82a8ce
ridoy113 <almamounhridoy113@gmail.com>
2022-04-18 02:34:10 +0600

    footer done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <footer>
-            <p><small>copyright @ /dynamic year/</small></p>
+        <footer className='footer'>
+            <div className='footer-text'>
+                <p><small>COPYRIGHT © /dynamic year/</small></p>
+                <p><small>A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic. Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs. This is because paragraphs show a reader where the subdivisions of an essay begin and end, and thus help the reader see the organization of the essay and grasp its main points.</small></p>
+            </div>
         </footer>
     );
 };
